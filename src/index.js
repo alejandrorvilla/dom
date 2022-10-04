de5ffe7ad7e710618a05084d021f143b123f5bcd
jaramirezv <jose.ramirez@openintl.com>
2022-10-04 18:32:13 -0500

    Cargar imagenes alaeatorias al mostrarse en la pantalla

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { subscribe } from "./lazy";
 
-console.log('Happy hacking :)')
+const main = document.querySelector('section')
+
+document.querySelector('button').addEventListener('click', () => {
+	const container = createImageNode();
+	main.appendChild(container);
+	subscribe(container);
+});
+
+function createImageNode() {
+	const container = document.createElement('div');
+	container.className = '';
+
+	const random = Math.floor(Math.random() * (122)) + 1;
+	const image = document.createElement('img');
+	image.className = 'mx-auto rounded-md bg-gray-300';
+	const src = `https://randomfox.ca/images/${random}.jpg`
+	image.width = '300';
+	image.src = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=';
+	image.dataset.src = src;
+
+	const span = document.createElement('span');
+	span.append(src);
+
+	container.append(image, span);
+
+	return container;
+}
+
+//Delegación de eventos
+main.addEventListener('click', (event) => {
+	const target = event.target;
+	if(target.nodeName === 'IMG') {
+		console.log(`Url: ${target.dataset.src}`)
+	}
+});
